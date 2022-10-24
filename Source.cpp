9a68059966a16ec81d4674555581346e6f62fedf
romantrety <114093642+22057221@users.noreply.github.com>
2022-10-24 18:38:04 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// 1
+	//int n;
+	//cin >> n;
+	//for (int i = 1; i <= n; i += 1) {
+		//cout << "@" << "             ";
+
+	//}
+//}
+
+
+
+
+	// 2
+	//int sum = 0, x, y, i;
+	//cout << "Vvedite x: ";
+	//cin >> x;
+	//cout << "Vvedite y: ";
+	//cin >> y;
+	//i = x;
+	//while (i <= y) {
+		//if (i % 2 == 0) {
+			//sum += i;
+		//}
+		//i++;
+	//}
+	//cout << sum << endl;
+//}
+
+
+
+
+	//
+	//int x, y, i;
+	//cin >> y;
+	//cin >> x;
+	//for (i < y; i++;) {
+	//}
+//}
+
+
+
+	//cout << "   ###################" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;
+	//cout << "   #                 #" << endl;j
+	//cout << "   ###################" << endl;
+//}
+
+
+
+	//for (int i = 0; i < 20; i++) cout << '#';
+	//cout << endl;
+	//for (int i = 0; i < 6; i++)
+	//{
+		//cout << '#';
+		//for (int j = 0; j < 18; j++) cout << ' ';
+		//cout << endl;
+	//}
+	//for (int i = 0; i < 20; i++) cout << '#';
+	//cout << endl;
+
+	//18 «адано натуральное число N. Ќайти количество натуральных чисел, не превосход€щих N и не дел€щихс€ ни на одно из чисел 2,3,5.
+	//int a;
+	//int jank = 0;
+	//cin >> a;
+	//for (int i = 0; i < a; i++) {
+		//if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+			//jank++;
+	//}
+	//cout << "  " << jank << endl;
+//}
+
+	
+
+
+
+
+
+
